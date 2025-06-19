bd57d875c8bbc32b9e901c5908da841f78e0f56e
Albert Volkman <albert.volkman@gmail.com>
2025-06-19 17:35:46 -0400

    Move pages to folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,6 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     'assets': 'assets',
   });
+  eleventyConfig.setIncludesDirectory('_includes');
+  eleventyConfig.setLayoutsDirectory('_layouts');
 };
